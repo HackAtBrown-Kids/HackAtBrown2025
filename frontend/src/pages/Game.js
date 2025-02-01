98d66a1976e43b405d49d93bd0a08991622a237f
Aanya Agrawal <purplishwings@gmail.com>
2025-02-01 17:00:35 -0500

    game template code

---
+++ frontend/src/pages/Game.js
@@ -0,0 +1,12 @@
+// dict question to an array of options
+import React, { useState, useEffect, useRef } from 'react';
+
+const Game = () => {
+	return (
+		<div>
+		Hello
+		</div>
+		);
+};
+
+export default Game;
